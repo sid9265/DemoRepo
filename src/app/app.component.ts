5c915459fdd4d46435e5ab7b996fb8fc8fc0ded0
Demo <r.hirpara@wishtreetech.com>
2021-07-07 19:18:50 +0530

    New commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-project';
-  name="";
+  
+  names: string | undefined;
+  age: number | undefined;
+  
+  
+  uName: string | undefined;
+  uAge: number | undefined; 
+
+  assignVal() {
+    this.uName = this.names;
+    this.uAge=this.age;
 }
+
+ clear() {
+  this.names = undefined;
+  this.age = undefined;
+
+  this.uName = this.names;
+  this.uAge=this.age;
+}
+    
+}
+
+ 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+
 @NgModule({
   declarations: [
     AppComponent
+    
   ],
   imports: [
     BrowserModule,
